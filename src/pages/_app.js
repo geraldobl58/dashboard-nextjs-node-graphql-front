83cf2280fa459e888c441ccf69fff73a880e80cb
Geraldo Luiz <geraldobl58@gmail.com>
2021-08-24 14:22:36 -0300

    :sparkles: Installing and Configuring Apollo Dev Tools

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { ApolloProvider } from '@apollo/client';
+import client from '../config/apollo';
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
